7351d96412cf1b41dbf6d1506f691853718c3e32
NolanSun <shr1213@live.com>
2018-06-01 21:30:10 +0800

    2018/6/1

--- 80日常监管/食品日常检查/test_food_daily_check.py
+++ 80日常监管/食品日常检查/test_food_daily_check.py
@@ -4,8 +4,7 @@ from food_daily_check import *
 
 class TestNewCheck(unittest.TestCase):
     """测试新建检查"""
-#    @unittest.skip("I don't want to run this case.")
-
+    @unittest.skip("I don't want to run this case.")
     def test_new_check(self):
         """Test method new_check()"""
         self.assertEqual(True, new_check())
@@ -13,15 +12,15 @@ class TestNewCheck(unittest.TestCase):
 
 class TestDoubleRandomTask(unittest.TestCase):
     """测试双随机任务"""
-    @unittest.skip("I don't want to run this case.")
+#    @unittest.skip("I don't want to run this case.")
+
     def test_double_random_task(self):
         self.assertEqual(True, double_random_task())
 
 
 class TestNormalTask(unittest.TestCase):
     """测试普通任务.py"""
-#    @unittest.skip("I don't want to run this case.")
-
+    @unittest.skip("I don't want to run this case.")
     def test_normal_task(self):
         self.assertEqual(True, normal_task())
 if __name__ == '__main__':
