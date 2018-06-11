7a2e33bcbc3b91bb9f64f0c943379f7cd9093cf6
NolanSun <shr1213@live.com>
2018-06-11 14:39:46 +0800

    2018/6/11

--- 80日常监管/食品日常检查/test_food_daily_check.py
+++ 80日常监管/食品日常检查/test_food_daily_check.py
@@ -23,26 +23,26 @@ class TestFoodDailyCheck(unittest.TestCase):
 #    @unittest.skip("I don't want to run this case.")
 
     def test_double_random_task(self):
-    """测试双随机任务"""
+        """测试双随机任务"""
         self.assertEqual(True, double_random_task())
 
     @unittest.skip("I don't want to run this case.")
     def test_normal_task(self):
-    """测试普通任务.py"""
+        """测试普通任务.py"""
         self.assertEqual(True, normal_task())
 
 
 #    @unittest.skip("I don't want to run this case.")
 
     def test_simple_check(self):
-    """测试简略检查.py"""
+        """测试简略检查.py"""
         self.assertEqual(True, simple_check())
 
 
 #    @unittest.skip("I don't want to run this case.")
 
     def test_new_template(self):
-    """测试普通任务.py"""
+        """测试普通任务.py"""
         self.assertEqual(True, new_template())
 if __name__ == '__main__':
     unittest.main()
