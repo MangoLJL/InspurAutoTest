3f4ea4050bdf58a67db6f35030c36f2e92a80f62
NolanSun <shr1213@live.com>
2018-06-19 11:40:53 +0800

    2018/6/19

--- 80日常监管/食品日常检查/food_actions.py
+++ 80日常监管/食品日常检查/food_actions.py
@@ -487,6 +487,10 @@ class NewNormalTask(object):
             enterprise_radio_button = WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located((By.XPATH, "//html//tr[1]/td[2]/input[1]")))
             enterprise_name = self.driver.find_element_by_xpath("//html//tr[1]/td[3]").text
             enterprise_radio_button.click()
+            try:
+                self.driver.find_element_by_id("radio0").click()  # 选择食品经营企业时会出现两个radio button
+            except:
+                pass
             self.button.click_save_button()
             time.sleep(1)
             self.driver.switch_to.default_content()
