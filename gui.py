96ba58ace0fbc5c0694145ffea0e8efb10b4a757
NolanSun <shr1213@live.com>
2018-07-02 18:43:22 +0800

    改为多线程模式

--- gui.py
+++ gui.py
@@ -22,16 +22,16 @@ class AutoTestGUI(BaseWidget):
         self.pull_from_github.value = self.pull_from_github_action_thread_button
 
         self.run_daily_check_test_suite = ControlButton('Run Daily Check Test Suite')
-        self.run_daily_check_test_suite.value = self.run_daily_check_test_suite_action
+        self.run_daily_check_test_suite.value = self.run_daily_check_test_suite_action_thread_button
 
         self.open_test_report = ControlButton('Open Test Report')
-        self.open_test_report.value = self.open_test_report_action
+        self.open_test_report.value = self.open_test_report_action_thread_button
 
         self.delete_test_report = ControlButton('Delete Test Report')
-        self.delete_test_report.value = self.delete_test_report_action
+        self.delete_test_report.value = self.delete_test_report_action_thread_button
 
         self.delete_screenshot = ControlButton('Delete Screenshot')
-        self.delete_screenshot.value = self.delete_screenshot_action
+        self.delete_screenshot.value = self.delete_screenshot_action_thread_button
 
         self.lable = ControlLabel('运行结果:')
         self.result_text = ControlLabel('结果将在此处显示...')
@@ -40,9 +40,25 @@ class AutoTestGUI(BaseWidget):
         self.formset = [('pull_from_github', 'run_daily_check_test_suite', 'open_test_report'), ('delete_test_report', 'delete_screenshot'), 'lable', 'result_text']
 
         self.pull_from_github_action_thread = threading.Thread(target=self.pull_from_github_action)
+        self.run_daily_check_test_suite_action_thread = threading.Thread(target=self.run_daily_check_test_suite_action)
+        self.open_test_report_action_thread = threading.Thread(target=self.open_test_report_action)
+        self.delete_test_report_action_thread = threading.Thread(target=self.delete_test_report_action)
+        self.delete_screenshot_action_thread = threading.Thread(target=self.delete_screenshot_action)
 
     def pull_from_github_action_thread_button(self):
         self.pull_from_github_action_thread.start()
+
+    def run_daily_check_test_suite_action_thread_button(self):
+        self.run_daily_check_test_suite_action_thread.start()
+
+    def open_test_report_action_thread_button(self):
+        self.open_test_report_action_thread.start()
+
+    def delete_test_report_action_thread_button(self):
+        self.delete_test_report_action_thread.start()
+
+    def delete_screenshot_action_thread_button(self):
+        self.delete_screenshot_action_thread.start()
 
     def pull_from_github_action(self):
         result = os.popen("cd C:\\Users\\Administrator\\Documents\\PythonAutoTest&&git pull")
