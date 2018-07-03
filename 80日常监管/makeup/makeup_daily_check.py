aa3d55f0c9a12beccf944d4aee738300982dc9df
NolanSun <shr1213@live.com>
2018-07-03 14:21:28 +0800

    2018/7/3

--- 80日常监管/makeup/makeup_daily_check.py
+++ 80日常监管/makeup/makeup_daily_check.py
@@ -45,7 +45,7 @@ def makeup_simple_check():
     makeup_simple_check_setup = Setup('http://10.12.1.80/portal/jsp/public/login.jsp')
     driver = makeup_simple_check_setup.setup_driver('liubx', '1', '智慧监管', '日常监管')
     try:
-        makeup_new_template_setup.choose_first_menu('化妆品监督检查')
+        makeup_simple_check_setup.choose_first_menu('化妆品监督检查')
         driver.find_element_by_id('000000000000000000000000019466').click()
         driver.find_element_by_id('check0301010001').click()
         switch_to_frame = SwitchToFrame(driver)
