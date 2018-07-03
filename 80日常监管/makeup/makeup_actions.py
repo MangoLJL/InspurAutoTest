10943808f0d941176beae8957ef30dd39c1674dd
NolanSun <shr1213@live.com>
2018-07-03 14:48:33 +0800

    2018/7/3

--- 80日常监管/makeup/makeup_actions.py
+++ 80日常监管/makeup/makeup_actions.py
@@ -53,7 +53,8 @@ class NewCheck(object):
     def third_step(self, checktype):
         check_type_button = WebDriverWait(self.driver, 10, 0.5).until(EC.presence_of_element_located((By.ID, checktype)))
         check_type_button.click()
-        self.driver.find_element_by_xpath("//i[@class='fa fa-search'][2]").click()
+        self.driver.find_element_by_xpath(
+            "//tr[@id='nametr2']//td[@class='fieldInput']//div[@class='input-group']//span[@class='input-group-addon']//i[@class='fa fa-search']").click()
         self.common_action.scroll_and_switch_to_iframe()
         collect_tab = WebDriverWait(self.driver, 20, 0.5).until(EC.presence_of_element_located((By.XPATH, "//a[@href='#collection']")))
         collect_tab.click()
