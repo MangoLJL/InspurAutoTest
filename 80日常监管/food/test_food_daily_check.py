a1ad3d4f330f98fffcd05ed72de97114050855b1
NolanSun <shr1213@live.com>
2018-07-18 10:17:02 +0800

    2018/7/18

--- 80日常监管/food/test_food_daily_check.py
+++ 80日常监管/food/test_food_daily_check.py
@@ -16,6 +16,7 @@ class TestFoodDailyCheck(unittest.TestCase):
     def tearDownClass(cls):
         try:
             template_ID = globalvar.get_value('food_template_ID')
+            print(template_ID)
             if template_ID == None:
                 print(time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time())) + '没有需要清理的食品模板...')
             else:
