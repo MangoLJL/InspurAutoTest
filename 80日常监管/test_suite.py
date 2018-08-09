9386d277c7c66e17d353e7f639b3896fec41a996
NolanSun <shr1213@live.com>
2018-08-09 15:24:04 +0800

    2018/8/9

--- 80日常监管/test_suite.py
+++ 80日常监管/test_suite.py
@@ -19,16 +19,16 @@ def job():
     print(time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time())) + '开始运行...')
     suite = unittest.TestSuite()
     tests = []
-    # tests.append(TestFoodDailyCheck("test_new_template"))  # 食品新建模板
-    # tests.append(TestFoodDailyCheck("test_simple_check"))  # 食品现场录入简易版本
+    tests.append(TestFoodDailyCheck("test_new_template"))  # 食品新建模板
+    tests.append(TestFoodDailyCheck("test_simple_check"))  # 食品现场录入简易版本
     # tests.append(TestFoodDailyCheck("test_new_check"))  # 食品现场录入全覆盖版本
-    # tests.append(TestFoodDailyCheck("test_double_random_task"))  # 食品双随机
-    # tests.append(TestFoodDailyCheck("test_normal_task"))  # 食品普通计划
+    tests.append(TestFoodDailyCheck("test_double_random_task"))  # 食品双随机
+    tests.append(TestFoodDailyCheck("test_normal_task"))  # 食品普通计划
     tests.append(TestFoodDailyCheck("test_save_draft"))  # 食品暂存草稿
-    # tests.append(TestMakeupDailyCheck("test_new_template"))  # 化妆品新建模板
-    # tests.append(TestMakeupDailyCheck("test_makeup_simple_check"))  # 化妆品现场录入简易版本
-    # tests.append(TestDrugDailyCheck("test_new_template"))  # 药品新建模板
-    # tests.append(TestDrugDailyCheck("test_drug_simple_check"))  # 药品现场录入建议版本
+    tests.append(TestMakeupDailyCheck("test_new_template"))  # 化妆品新建模板
+    tests.append(TestMakeupDailyCheck("test_makeup_simple_check"))  # 化妆品现场录入简易版本
+    tests.append(TestDrugDailyCheck("test_new_template"))  # 药品新建模板
+    tests.append(TestDrugDailyCheck("test_drug_simple_check"))  # 药品现场录入建议版本
 
     suite.addTests(tests)
 
