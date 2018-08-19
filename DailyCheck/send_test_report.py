62dc905e24c27ceacdaefb1af452875fc6c56d5d
NolanSun <shr1213@live.com>
2018-08-19 16:30:46 +0800

    2018/8/19

--- DailyCheck/send_test_report.py
+++ DailyCheck/send_test_report.py
@@ -11,8 +11,8 @@ subject = time.strftime('%Y-%m-%d_%H-%M-%S', time.localtime(time.time())) + 'Tes
 
 
 def send_text_report_html(test_report_path, sender_address, sender_password, receiver_address):
-	my_sender = sender_address
-	my_pass = sender_password
-	receiver_addr.append(receiver_address)
+    my_sender = sender_address
+    my_pass = sender_password
+    receiver_addr.append(receiver_address)
     MS = MailSender(my_sender, my_pass, sender_name, receiver_addr, subject)
     MS.send_html(test_report_path)
